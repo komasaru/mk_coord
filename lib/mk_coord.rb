38b9c24e86426cbc796e9c2c978651ed82184fe5
Masaru Koizumi <masaru@mk-mode.com>
2016-07-31 15:13:27 +0900

    UPD: Fixed the pol2rect method's logic.

--- lib/mk_coord.rb
+++ lib/mk_coord.rb
@@ -51,9 +51,11 @@ module MkCoord
 
   def self.pol2rect(pol, r)
     lambda, phi = pol
-    mtx = r_y(phi)
-    mtx = r_z(lambda, mtx)
-    return rotate(mtx, [r, 0.0, 0.0])
+    return [
+      r * Math.cos(lambda) * Math.cos(phi),
+      r * Math.sin(lambda) * Math.cos(phi),
+      r * Math.sin(phi)
+    ]
   rescue => e
     raise
   end
--- spec/mk_coord_spec.rb
+++ spec/mk_coord_spec.rb
@@ -65,7 +65,7 @@ describe MkCoord do
       subject { described_class.pol2rect(pos_pol, r) }
       it { expect(subject).to match([
         be_within(1.0e-16).of(0.9942990763065704),
-        be_within(1.0e-17).of(0.03815764032143803),
+        be_within(1.0e-17).of(-0.03815764032143803),
         be_within(1.0e-18).of(-0.016549487843225463)
       ]) }
     end
